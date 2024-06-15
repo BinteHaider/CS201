f8080bea2eb9ae08fd140693063019bb26b63f74
= <=>
2024-06-16 03:43:34 +0500

    hello

---
+++ classes/class 4/class 4.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+main(){
+
+
+/*
+Calculate the average age of a class of ten students.
+Prompt the user to enter the age of each student.*/
+
+main(){
+	
+	
+	int age1,age2,age3,age4,age5,age6,age7,age8,age9,age10=0;
+	int averageAge,totalAge;
+	
+// // take age of the student from user
+// 
+// cout << "please enter the age of student 1" << endl;
+// cin >> age1;
+// cout << "please enter the age of student 2"<< endl;
+// cin >> age2;
+// cout << "please enter the age of student 3" << endl;
+// cin >> age3;
+// cout << "please enter the age of student 4" << endl;
+// cin >> age4;
+// cout << "please enter the age of student 5" << endl;
+// cin >> age5;
+// cout << "please enter the age of student 6" << endl;
+// cin >> age6;
+// cout << "please enter the age of student 7"<< endl;
+// cin >> age7;
+// cout << "please enter the age of student 8" << endl;
+// cin >> age8;
+// cout << "please enter the age of student 9" << endl;
+// cin >> age9;
+// cout << "please enter the age of student 10" << endl;
+// cin >> age10;
+// 
+// totalAge = age1+age2+age3+age4+age5+age6+age7+age8+age9+age10;
+// 
+// averageAge = totalAge / 10;
+// 
+// cout << averageAge;
+
+
+//-----------------------------------------------------------------------------------------------
+
+
+
+}
